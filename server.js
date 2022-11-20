8242decdd2532d78060b74561db88fe124d8cd8a
Дмитрий <leon6024@yandex.ru>
2022-11-20 10:46:51 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const exphbs = require('express-handlebars')
-//const todoRoutes = require('./routes/todos')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000  
 
 const app = express()                                    
 const hbs = exphbs.create({
@@ -19,27 +18,14 @@ app.set('views', 'views')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-/*  app.use(todoRoutes)
-
-async function start() {
+async function start() { 
   try {
-    await mongoose.connect(
-      'mongodb+srv://vladilen:1q2w3e4r@cluster0-ua4e7.mongodb.net/todos',
-      {
-        useNewUrlParser: true,
-        useFindAndModify: false
-      }
-    )
     app.listen(PORT, () => {
-      console.log('Server has been started...')
+      console.log('Сервер запустился!)')
     })
   } catch (e) {
     console.log(e)
   }
-}*/
+}
 
-app.listen(PORT, () => {
-  console.log('Server has been started...')
-})
-
-//start()
+start()
