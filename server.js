4b1bd85067b0e66f3cc3125a0c79cc3bc838b364
Дмитрий <leon6024@yandex.ru>
2022-11-28 18:41:39 +0300

    Сервер подключение

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
+
+const PORT = process.env.PORT || 3000  
+
+const app = express()                                    
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+async function start() { 
+  try {
+    app.listen(PORT, () => {
+      console.log('Сервер запустился!')
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
